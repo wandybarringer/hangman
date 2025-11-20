05036dac6370097040427273e6699f677b176944
William <wabarringer@gmail.com>
2025-11-20 13:39:32 -0800

    Javascript first draft finished

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var startButtonEl = document.querySelector('.start');
 var blanksEl = document.querySelector('.blanks');
+var winsEl = document.querySelector('#wins');
+var lossesEl = document.querySelector('#losses');
+var timerEl = document.querySelector('#seconds-remaining');
+var resetEl = document.querySelector('.reset');
+var hardEl = document.querySelector('#hard');
+var normalEl = document.querySelector('#normal');
+var easyEl = document.querySelector('#easy');
 
-// TODO: create an array of words to guess
+// TODO: create an array of words to guess ✅
 var pokemonNames = [
   'pikachu',
   'charizard',
@@ -18,6 +25,26 @@ var pokemonNames = [
 var chosenWord;
 var blanks;
 var letters;
+var secondsLeft = 0;
+var difficulty = 'normal';
+
+// TODO: create difficulty function
+function difficultySelection() {
+  hardEl.addEventListener('click', function () {
+    difficulty = 'hard';
+  });
+  normalEl.addEventListener('click', function () {
+    difficulty = 'normal';
+  });
+  easyEl.addEventListener('click', function () {
+    difficulty = 'easy';
+  });
+}
+
+// TODO: Call scores from local storage so they are displayed on refresh ✅
+// assigning wins and losses to what is store in local storage, or setting to zero if there's nothing in local storage
+var wins = localStorage.getItem('wins') || 0;
+var losses = localStorage.getItem('losses') || 0;
 
 // start the game by clicking on a button.
 // try and guess a word by filling in a number of blanks that match the number of letters in that word.
@@ -40,16 +67,48 @@ function addBlanks() {
   blanksEl.textContent = displayedBlanks;
 }
 
-startButtonEl.addEventListener('click', function () {
-  // determining the word to play
-  var index = Math.floor(Math.random() * pokemonNames.length);
-  chosenWord = pokemonNames[index];
-  addBlanks();
-  console.log(chosenWord);
+// see total wins and losses on the screen.
+// TODO: track wins and loses, append to DOM ✅
+function addWinLoss() {
+  winsEl.textContent = wins;
+  lossesEl.textContent = losses;
+}
 
-  // When a user presses a letter key, the user's guess should be captured as a key event.
-  // When a user correctly guesses a letter, the corresponding blank "_" should be replaced by the letter. For example, if the user correctly selects "a", then "a _ _ a _" should appear.
-  // TODO: create a function that listens to keydown events and replaces any blanks with the letter if it is correct
+// TODO: reset the timer to 0
+// win the game when I have guessed all the letters in the word.
+// lose the game when the timer runs out before I have guessed all the letters.
+// When a user wins or loses a game, a message should appear and the timer should stop.
+// TODO: create a function that determines winning conditions based on letters guessed or losing conditions based on timer ✅
+// the game is timed.
+// TODO: create a timer that starts at 10 ✅
+function timer() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+
+    if (blanks.join('') === letters.join('')) {
+      clearInterval(timerInterval);
+      blanksEl.textContent = 'YOU WIN';
+      wins++;
+      //   re-enables the start button
+      startButtonEl.disabled = false;
+    } else if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      losses++;
+      startButtonEl.disabled = false;
+    }
+    timerEl.textContent = secondsLeft;
+    addWinLoss();
+    // When a user refreshes or returns to the brower page, the win and loss counts should persist.
+    // TODO: save score to local storage
+    localStorage.setItem('wins', wins);
+    localStorage.setItem('losses', losses);
+  }, 1000);
+}
+
+// When a user presses a letter key, the user's guess should be captured as a key event.
+// When a user correctly guesses a letter, the corresponding blank "_" should be replaced by the letter. For example, if the user correctly selects "a", then "a _ _ a _" should appear.
+// TODO: create a function that listens to keydown events and replaces any blanks with the letter if it is correct ✅
+function checkKeys() {
   document.addEventListener('keydown', function (event) {
     console.log(event.key);
     // TODO: figure out how to find the index of the letter key pressed in the chosenWord and replace it on the blanks ✅
@@ -62,19 +121,49 @@ startButtonEl.addEventListener('click', function () {
     }
     // re-appending the blanks string to the html with the space seperators
     blanksEl.textContent = blanks.join(' ');
+    console.log(blanks);
+    console.log(letters);
   });
-});
+}
 
-// the game is timed.
-// TODO: create a timer that starts at 10
+function play() {
+  startButtonEl.addEventListener('click', function () {
+    // edge case: user clicks button multiple times, which increases the interval
+    startButtonEl.disabled = true;
+    // starting at 10 seconds left
+    if (difficulty === 'hard') {
+      secondsLeft = 5;
+    } else if (difficulty === 'normal') {
+      secondsLeft = 10;
+    } else {
+      secondsLeft = 20;
+    }
+    timerEl.textContent = secondsLeft;
+    // determining the word to play
+    var index = Math.floor(Math.random() * pokemonNames.length);
+    chosenWord = pokemonNames[index];
+    addBlanks();
+    checkKeys();
+    timer();
 
-// win the game when I have guessed all the letters in the word.
-// lose the game when the timer runs out before I have guessed all the letters.
-// When a user wins or loses a game, a message should appear and the timer should stop.
-// TODO: create a function that determines winning conditions based on letters guessed or losing conditions based on timer
+    console.log(letters);
+    console.log(blanks);
+  });
+}
 
-// see total wins and losses on the screen.
-// TODO: track wins and loses, append to DOM
+// TODO: Reset button to clears wins ✅
+function reset() {
+  resetEl.addEventListener('click', function () {
+    wins = 0;
+    losses = 0;
+    winsEl.textContent = wins;
+    lossesEl.textContent = losses;
+    localStorage.setItem('wins', wins);
+    localStorage.setItem('losses', losses);
+  });
+}
 
-// When a user refreshes or returns to the brower page, the win and loss counts should persist.
-// TODO: save score to local storage
+difficultySelection();
+addWinLoss();
+play();
+reset();
